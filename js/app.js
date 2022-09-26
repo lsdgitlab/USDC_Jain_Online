470714dba0e0de5a940e6a140afe9cd5622e956f
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-09-26 19:07:53 +0530

    new css file named media added, mixin, reusable, root, variable, style css files updated, index page updated with new sections,on app js scripts for counter & ranking slider added.

--- js/app.js
+++ js/app.js
@@ -4,3 +4,29 @@ $(".accredit-carousel.owl-carousel").owlCarousel({
     dots:false,mouseDrag:true, smartSpeed: 2000,
     // navContainer: '.top-highlights .custom-nav',
     responsiveClass:true,responsive:{0:{items:2,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}});
+
+$(".corporate-carousel.owl-carousel").owlCarousel({
+    margin:0,autoplay:false,nav:false,
+    // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
+    dots:false,mouseDrag:true, smartSpeed: 2000,
+    // navContainer: '.top-highlights .custom-nav',
+    responsiveClass:true,responsive:{0:{items:2,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:5}}});    
+
+$(document).ready(function () {
+    // start script to count the numbers from 0 to specific number 
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    // end script to count the numbers from 0 to specific number    
+
+})
+
+ 
