2c9cd34672a6c11f97196c70080e8c54878e3833
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-09-27 18:56:41 +0530

    new sections aded in html page, added images, media,mixin,reusable,root,variable,style, js/app.js updated

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
+$(".rank-carousel.owl-carousel").owlCarousel({
+    margin:0,autoplay:false,nav:false,
+    // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
+    dots:false,mouseDrag:true, smartSpeed: 2000,
+    // navContainer: '.top-highlights .custom-nav',
+    responsiveClass:true,responsive:{0:{items:1,touchDrag:true,nav:false,dots:true},600:{items:2,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}
+})
+
 $(".accredit-carousel.owl-carousel").owlCarousel({
     margin:0,autoplay:false,nav:false,
     // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
     dots:false,mouseDrag:true, smartSpeed: 2000,
     // navContainer: '.top-highlights .custom-nav',
-    responsiveClass:true,responsive:{0:{items:2,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}});
+    responsiveClass:true,responsive:{0:{items:1,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}});
 
 $(".corporate-carousel.owl-carousel").owlCarousel({
     margin:0,autoplay:false,nav:false,
     // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
     dots:false,mouseDrag:true, smartSpeed: 2000,
     // navContainer: '.top-highlights .custom-nav',
-    responsiveClass:true,responsive:{0:{items:2,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:5}}});    
+    responsiveClass:true,responsive:{0:{items:2,touchDrag:true,nav:false,dots:true},480:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:5}}});    
 
 $(document).ready(function () {
     // start script to count the numbers from 0 to specific number 
