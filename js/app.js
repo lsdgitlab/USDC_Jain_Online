a4f8e0a8e193aa49d11a69cbb611045be72b000f
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-09-30 10:53:16 +0530

    added new sections on index page, normalize css added in vendors, also added css with some changes, script added for learner& tabbing-accordian sections and changes

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ $(".rank-carousel.owl-carousel").owlCarousel({
     responsiveClass:true,responsive:{0:{items:1,touchDrag:true,nav:false,dots:true},600:{items:2,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}
 })
 
-$(".accredit-carousel.owl-carousel").owlCarousel({
-    margin:0,autoplay:false,nav:false,
+$(".learner-carousel.owl-carousel").owlCarousel({
+    margin:10,autoplay:false,nav:false,
     // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
-    dots:false,mouseDrag:true, smartSpeed: 2000,
+    dots:true,mouseDrag:true,touchDrag:true,smartSpeed: 2000,
     // navContainer: '.top-highlights .custom-nav',
-    responsiveClass:true,responsive:{0:{items:1,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}});
+    responsiveClass:true,responsive:{0:{items:1},768:{items:1},992:{items:1}}});    
 
 $(".corporate-carousel.owl-carousel").owlCarousel({
     margin:0,autoplay:false,nav:false,
@@ -19,6 +19,13 @@ $(".corporate-carousel.owl-carousel").owlCarousel({
     dots:false,mouseDrag:true, smartSpeed: 2000,
     // navContainer: '.top-highlights .custom-nav',
     responsiveClass:true,responsive:{0:{items:2,touchDrag:true,nav:false,dots:true},480:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:5}}});    
+
+$(".accredit-carousel.owl-carousel").owlCarousel({
+    margin:0,autoplay:false,nav:false,
+    // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
+    dots:false,mouseDrag:true, smartSpeed: 2000,
+    // navContainer: '.top-highlights .custom-nav',
+    responsiveClass:true,responsive:{0:{items:1,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}});
 
 $(document).ready(function () {
     // start script to count the numbers from 0 to specific number 
@@ -33,8 +40,43 @@ $(document).ready(function () {
             }
         });
     });
-    // end script to count the numbers from 0 to specific number    
+    // end script to count the numbers from 0 to specific number 
+    
+    // start script fot tabbing
+    $('.cat-list li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('.cat-list li').removeClass('current');
+        $('.accord-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    })
+
+    // end script for tabbing
+
+    
+//start script for accordian 
+$(function() {
+    $('.accordtitle').click(function(j) {
+  
+      var dropDown = $(this).closest('.accordbx').find('.accorddesc');
+      $(this).closest('.accord').find('.accorddesc').not(dropDown).slideUp();
+  
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+      } else {
+        $(this).closest('.accord').find('.accordtitle.active').removeClass('active');
+        $(this).addClass('active');
+      }
+  
+      dropDown.stop(false, true).slideToggle();
+      j.preventDefault();
+    });
+  });
+  //end script for accordian
 
 })
+
 
  
