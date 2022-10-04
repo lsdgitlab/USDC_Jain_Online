71748294caa7333a08012f45b70da605b64d8355
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-10-04 18:48:38 +0530

    animations added and new animation css file added in sass folder

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
 $(".rank-carousel.owl-carousel").owlCarousel({
-    margin:0,autoplay:false,nav:false,
+    margin:0,nav:false,
     // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
-    dots:false,mouseDrag:true, smartSpeed: 2000,
+    dots:false,mouseDrag:true, 
+    autoplay:true, autoplayTimeout:5000, autoplayHoverPause:true, smartSpeed: 3000,
     // navContainer: '.top-highlights .custom-nav',
     responsiveClass:true,responsive:{0:{items:1,touchDrag:true,nav:false,dots:true},600:{items:2,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}
 })
 
 $(".learner-carousel.owl-carousel").owlCarousel({
-    margin:10,autoplay:false,nav:false,
+    margin:10, nav:false,
     // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
-    dots:true,mouseDrag:true,touchDrag:true,smartSpeed: 2000,
+    dots:true,mouseDrag:true,touchDrag:true,
+    autoplay:true, autoplayTimeout:3000, autoplayHoverPause:true, smartSpeed: 5500,
     // navContainer: '.top-highlights .custom-nav',
     responsiveClass:true,responsive:{0:{items:1},768:{items:1},992:{items:1}}});    
 
 $(".corporate-carousel.owl-carousel").owlCarousel({
-    margin:0,autoplay:false,nav:false,
+    margin:0,nav:false,
     // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
-    dots:false,mouseDrag:true, smartSpeed: 2000,
+    dots:false,mouseDrag:true, 
+    autoplay:true, autoplayTimeout:5000, autoplayHoverPause:true, smartSpeed: 3000,
     // navContainer: '.top-highlights .custom-nav',
     responsiveClass:true,responsive:{0:{items:2,touchDrag:true,nav:false,dots:true},480:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:5}}});    
 
 $(".accredit-carousel.owl-carousel").owlCarousel({
-    margin:0,autoplay:false,nav:false,
+    margin:0,nav:false,
     // navText:["<img src='images/arrow-prev.png' alt='prev'>","<img src='images/arrow-next.png' alt='next'>"],
     dots:false,mouseDrag:true, smartSpeed: 2000,
+    autoplay:true, autoplayTimeout:5000, autoplayHoverPause:true, smartSpeed: 3000,
     // navContainer: '.top-highlights .custom-nav',
     responsiveClass:true,responsive:{0:{items:1,touchDrag:true,nav:false,dots:true},600:{items:3,touchDrag:true,nav:false,dots:true},768:{items:3,touchDrag:true,nav:false,dots:true},800:{items:3,touchDrag:true,nav:false,dots:true},992:{items:4}}});
 
