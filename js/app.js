69e8b19e2dfbcf7e8640a6c484ed3a9a71611b60
Ls-prafulla <prafulla.patil@lsdigital.com>
2022-10-21 12:43:44 +0530

    at footer sticky section added and read more read less functionality added

--- js/app.js
+++ js/app.js
@@ -98,5 +98,18 @@ $('.enquire-now a').click(function(){
 });
 
 
-
+$(document).ready(function() {
+    $("#toggle").click(function() {
+      var elem = $("#toggle").text();
+      if (elem == "Read More") {
+        //Stuff to do when btn is in the read more state
+        $("#toggle").text("Read Less");
+        $("#text").slideDown();
+      } else {
+        //Stuff to do when btn is in the read less state
+        $("#toggle").text("Read More");
+        $("#text").slideUp();
+      }
+    });
+  });
  
